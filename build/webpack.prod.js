11e9d07b4717da582e937dc0f58e41a843074f4a
cnbjlhkj－5747a <lhkj@cnbjlhkj5747adeMacBook-Pro.local>
2018-05-04 21:08:46 +0800

    添加生产环境打包

---
+++ build/webpack.prod.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const config = {
+  mode: 'production',
+  resolve: {
+    extensions: [".js", ".json", ".vue", ".css"],
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: [path.join(__dirname, '../src')],
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        loader: 'file-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+        include: [path.join(__dirname, '../src')],
+      },
+    ],
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      filename: path.join(__dirname, '../dist', 'index.html'),
+      template: path.join(__dirname, '../src', 'index.html'),
+      inject: true,
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: "[name].css",
+    })
+  ],
+};
+
+module.exports = config;
